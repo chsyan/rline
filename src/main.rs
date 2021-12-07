788890c2919c449045d58d9dbbd98eb9de5a457a
chsyan <charles5850@gmail.com>
2021-12-06 19:14:41 -0800

    Error handling

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,51 @@
-use std::{env, fs};
+use std::{env, fs, process, error};
 use rand::Rng;
 
-fn main() -> std::io::Result<()> {
+fn main() {
     let args: Vec<String> = env::args().collect();
-    let input_filename = &args[1];
-    let output_filename = &args[2];
-    let input_file = fs::read_to_string(input_filename).unwrap();
-    let mut input_lines: Vec<&str> = input_file.lines().collect();
-    let mut output_lines: Vec<&str> = Vec::with_capacity(input_lines.len());
-    while input_lines.len() > 0 {
-        let line = &input_lines.remove(rand::thread_rng().gen_range(0..input_lines.len()));
-        output_lines.push(line);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Error parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    if let Err(err) = run(config) {
+        println!("Runtime error: {}", err);
+        process::exit(1);
+
+    }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn error::Error>> {
+    let in_str = fs::read_to_string(config.in_file).unwrap();
+    let mut in_lines: Vec<&str> = in_str.lines().collect();
+    let mut out_lines: Vec<&str> = Vec::with_capacity(in_lines.len());
+    while in_lines.len() > 0 {
+        let line = &in_lines.remove(rand::thread_rng().gen_range(0..in_lines.len()));
+        out_lines.push(line);
         println!("{}", line);
     }
-    let mut output_file = output_lines.join("\n");
-    output_file.push_str("\n");
-    fs::write(output_filename, output_file)?;
+    let mut out_str = out_lines.join("\n");
+    out_str.push_str("\n");
+    fs::write(config.out_file, out_str)?;
     Ok(())
+}
+
+
+struct Config {
+    in_file: String,
+    out_file: String,
+}
+
+
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments");
+        }
+
+        let in_file = args[1].clone();
+        let out_file = args[2].clone();
+
+        Ok(Config { in_file, out_file })
+    }
 }
